77738748f994d53c759902e95a40e23aae136b40
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2023-03-08 21:55:21 +0700

    Update lin1.py

--- Data/lin1.py
+++ Data/lin1.py
@@ -1,1 +1,35 @@
+import os,sys,json,time
+import threading
+from rich.table import Table as me
+from rich.console import Console as sol
+from bs4 import BeautifulSoup as parser
+from concurrent.futures import ThreadPoolExecutor as tred
+from rich.console import Group as gp
+from rich.panel import Panel as nel
+from rich import print as cetak
+from rich.markdown import Markdown as mark
+from rich.columns import Columns as col
 
+def update():
+  os.system('git pull')
+def clear():
+  os.system('clear')
+def banner():
+	print('''
+      	\x1b[1;93m_______           ______   _______  _                 ______  
+       (  ____ )|\     /|(  __  \ (  ___  )( \      |\     /|(  __  \ 
+       | (    )|| )   ( || (  \  )| (   ) || (      ( \   / )| (  \  )
+       | (____)|| |   | || |   ) || (___) || | _____ \ (_) / | |   ) |
+       |     __)| |   | || |   | ||  ___  || |(_____) ) _ (  | |   | |
+       | (\ (   | |   | || |   ) || (   ) || |       / ( ) \ | |   ) |
+       | ) \ \__| (___) || (__/  )| )   ( || (____/\( /   \ )| (__/  )
+       |/   \__/(_______)(______/ |/     \|(_______/|/     \|(______/ 
+       ''')
+ 
+class menu:
+    poi='\x1b[1;92myang murah itu rendah hati\n\n\x1b[1;93myang mahal itu harga diri'
+    pol=nel(poi,style='green')
+    cetak(nel(pol,style='cyan'))
+    
+if __name__=='__main__':
+    menu()
